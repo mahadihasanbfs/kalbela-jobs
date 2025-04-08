8ab041c8917ab8dffe8eadcc7a4a061694d8fb31
Md Nahid <mdnahid360s@gmail.com>
2025-04-08 19:57:45 +0600

    fix 2

--- app/(home)/components/FeaturedJobs.tsx
+++ app/(home)/components/FeaturedJobs.tsx
@@ -41,7 +41,7 @@ const FeaturedJobs: React.FC = () => {
 
                               <div className="flex flex-col gap-2">
                                     <Image
-                                          src="/banner4.jpg"
+                                          src="/assets/banner3.jpg"
                                           alt="ad"
                                           width={500}
                                           height={500}
