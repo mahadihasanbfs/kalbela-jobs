19e3bec4dfa6af81c527677c0862e79ec3bf11ce
nazmulsujon <sujon686500@gmail.com>
2025-04-11 12:49:33 +0600

    about us page

--- app/(home)/contact/page.tsx
+++ app/(home)/contact/page.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ContactForm from './components/ContactForm';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import MapView from './components/MapView';
+import PageBanner from '@/components/PageBanner';
 
 const page = () => {
   return (
     <div>
-      <div className='relative'>
-        <img src="/contact_us_bg.jpg" alt="" />
-        <div className='text-white text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase'>
-          <h1 className='text-3xl font-bold mb-2'>Contact Us</h1>
-          <p className='font-medium uppercase'>We Are Open to Discuss Your Needs</p>
-        </div>
-      </div>
+      <PageBanner image='/contact_us_bg.jpg' title='Contact Us' description='We Are Open to Discuss Your Needs' />
       <MaxWidthWrapper className='my-10'>
         <ContactForm />
       </MaxWidthWrapper>
