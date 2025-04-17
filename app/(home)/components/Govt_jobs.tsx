8279511dd0020243f3da2ab9e60b5abeea951804
Md Nahid <76812306+MdNahid360@users.noreply.github.com>
2025-04-17 18:05:35 +0600

    Update Govt_jobs.tsx

--- app/(home)/components/Govt_jobs.tsx
+++ app/(home)/components/Govt_jobs.tsx
@@ -19,12 +19,12 @@ const GovJob: React.FC = () => {
   const limit = 6;
 
   const { data, loading, error } = useApiRequest<any>(
-    `jobs/get-all-govt-jobs?page=${page}&limit=${limit}`,
+    `jobs/get-all-org-jobs?page=${page}&limit=${limit}`,
     'GET'
   );
 
-  const jobs = data?.data?.jobs || [];
-  const totalPages = data?.data?.pagination?.totalPages || 1;
+  const jobs = data?.data || [];
+  const totalPages = data?.data?.total_pages || 1;
 
   if (loading) {
     return (
@@ -86,14 +86,14 @@ const GovJob: React.FC = () => {
       </div>
     )
   }
-
+  console.log('job', data);
   return (
     <div className='bg-cover'
       style={{
         backgroundImage: `linear-gradient(180deg, #ffffffef, #ffffffef), url(/jobbg.jpeg)`,
       }}
     >
-      <div className="md:col-span-3 md:border flex flex-col justify-center">
+      <div className="md:col-span-3 md:border flex pb-4 flex-col justify-center">
         <JobTitleBar
           title="Government Jobs"
           iconSrc="https://image.kalbelajobs.com/api/v1/image/679674886283397bf670bc7d.png"
@@ -105,9 +105,42 @@ const GovJob: React.FC = () => {
           {jobs.length > 0 && <div className="grid md:px-4 md:h-[300px] px-0 f-full md:gap-4 gap-2 grid-cols-2 lg:grid-cols-3">
             {
               jobs?.map((job: any) => (
-                <div key={job._id}>
-                  <JobCard2 job={job} />
-                </div>
+                <Link
+                  key={job._id}
+                  href={`${job?.org_website}`}
+                  className="group relative flex bg-white justify-start flex-col md:flex-row w-full items-start gap-2 overflow-hidden hover:bg-gray-50 rounded border md:p-3 h-[100px] p-2 shadow-sm transition-all hover:border-gray-300"
+                >
+                  <div className="md:block md:w-auto flex w-full justify-center">
+                    <div className="h-16 w-16 m-auto bg-white">
+                      {job?.logo ? (
+                        <img
+                          className="h-full w-20 rounded border border-gray-300 bg-white object-contain p-2"
+                          src={job.logo}
+                          alt={job.name || 'Company Logo'}
+                          onError={(e) => (e.currentTarget.src = '/fallback_img.png')}
+                        />
+                      ) : (
+                        <div className="flex justify-center items-center h-full rounded border-2 border-gray-300 bg-white p-2 shadow-md">
+                          <span className="text-xl font-semibold text-gray-600">
+                            {job?.name?.charAt(0).toUpperCase() || 'C'}
+                          </span>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+
+                  <div className="flex-grow gap-1 text-center md:text-start mx-auto">
+                    <h3 className="font-semibold text-sm capitalize group-hover:text-blue-500">
+                      <h3 className="font-semibold text-sm capitalize group-hover:text-blue-500">
+                        {job?.name?.length > 65
+                          ? job?.name.slice(0, 65) + "..."
+                          : job?.name}
+                      </h3>
+
+                    </h3>
+                    <p className="text-xs">{job?.org_website}</p>
+                  </div>
+                </Link>
               ))
             }
           </div>}
